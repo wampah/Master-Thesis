0db58b93eff76781ee0e28ccde715fd6b56e1597
wampah <jp.reyesf@uniandes.edu.co>
2024-02-27 17:25:11 -0500

    Agrega setup

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(name='5barras',
+      version='0.1',
+      install_requires=['gymnasium',
+                       'numpy']) # And any other dependencies required
