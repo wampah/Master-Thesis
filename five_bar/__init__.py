b2f5341805409c727e550166e25d1a202bc32eb0
wampah <jp.reyesf@uniandes.edu.co>
2024-05-07 18:02:58 -0500

    Agrega el generador de posiciones, aún no funciona

--- five_bar/__init__.py
+++ five_bar/__init__.py
@@ -1,5 +1,5 @@
 from gymnasium.envs.registration import register
 register(id='five_bar-v0',
          entry_point='five_bar.envs.fivebar_reacher:FiveBar_Reacher',
-         max_episode_steps=50,
+         max_episode_steps=100,
         reward_threshold=-25,)
