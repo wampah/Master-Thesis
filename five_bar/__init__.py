560c965eea993200576e122f948cd7386efd80b1
wampah <jp.reyesf@uniandes.edu.co>
2025-03-05 13:38:11 -0500

    Modifies timesteps

--- five_bar/__init__.py
+++ five_bar/__init__.py
@@ -1,5 +1,5 @@
 from gymnasium.envs.registration import register
 register(id='five_bar-v0',
          entry_point='five_bar.envs.fivebar_reacher:FiveBar_Reacher',
-         max_episode_steps=100,
+         max_episode_steps=30,
         reward_threshold=1000000,)
