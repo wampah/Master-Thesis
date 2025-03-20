285d64158b97337fa5ea3c542beb8a0efab0e313
Juan Pablo Reyes <juan.pa.r@hotmail.com>
2025-03-20 17:04:48 -0500

    Modify steps per episode and removes threshold

--- five_bar/__init__.py
+++ five_bar/__init__.py
@@ -1,5 +1,4 @@
 from gymnasium.envs.registration import register
 register(id='five_bar-v0',
          entry_point='five_bar.envs.fivebar_reacher:FiveBar_Reacher',
-         max_episode_steps=30,
-        reward_threshold=1000000,)
+         max_episode_steps=50,)
